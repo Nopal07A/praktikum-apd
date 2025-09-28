8a276cdfb0bc75c2139026fa22d35d52a16b44b3
Nopal07A <mnaufalal101@gmail.com>
2025-09-28 08:18:15 +0800

    apd pt 3

---
+++ post-test/post-test-apd-3/2509106050-Muhammad Naufal Abimar-PT-3.py
@@ -0,0 +1,16 @@
+username = "nopal"
+Password = "050"
+
+ussername = input("masukkan ussername anda:")
+password = input ("masukkan password anda:")
+
+if ussername == username and password == Password:
+    print("login berhasil")
+elif username == ussername and Password != password:
+    print("password anda salah")
+elif username != ussername and Password == password:
+    print("username anda salah")
+else:
+    print ("login gagal cek username dan password anda")
+
+
