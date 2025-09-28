27c7a186e53a00a472f519cc8ab7230936ab4bd7
Nopal07A <mnaufalal101@gmail.com>
2025-09-28 22:32:57 +0800

    pt 3 apd

--- post-test/post-test-apd-3/2509106050-Muhammad Naufal Abimar-PT-3.py
+++ post-test/post-test-apd-3/2509106050-Muhammad Naufal Abimar-PT-3.py
@@ -13,4 +13,149 @@ elif username != ussername and Password == password:
 else:
     print ("login gagal cek username dan password anda")
 
+print("=== Program Konversi Satuan ===")
+print("1. konversi Panjang")
+print("2. konversi Massa")
+print("3. konversi Suhu")
+print("4. konversi Waktu")
+print("5. konversi mata uang")
+
+konversi = input("Pilih konversi yang di inginkan (1-5): ")
+
+if konversi == "1":
+    print("--- Konversi Panjang ---")
+    print("a. Feet -> Meter")
+    print("b. Kilometer -> Meter")
+    print("c. Centimeter -> Meter")
+    pilihan = input("Pilih konversi (a - c): ")
+
+    if pilihan == "a":
+        nilai = float(input("Masukkan nilai (feet): "))
+        hasil = nilai * 0.305
+        print(nilai, "feet =", hasil, "meter")
+    elif pilihan == "b":
+        nilai = float(input("Masukkan nilai (km): "))
+        hasil = nilai * 1000
+        print(nilai, "km =", hasil, "meter")
+    elif pilihan == "c":
+        nilai = float(input("Masukkan nilai (cm): "))
+        hasil = nilai / 100
+        print(nilai, "cm =", hasil, "meter")
+    else:
+        print("Pilihan tidak ada.")
+
+elif konversi == "2":
+    print("--- Konversi Massa ---")
+    print("a. Pound -> Kilogram")
+    print("b. Ton -> Kilogram")
+    print("c. Gram -> Kilogram")
+    print("d. hektogram -> kilogram")
+    print("e. dekagram -> kilogram")
+    pilihan = input("Pilih konversi (a - c): ")
+
+    if pilihan == "a":
+        nilai = float(input("Masukkan nilai (pound): "))
+        hasil = nilai * 0.454
+        print(nilai, "pound =", hasil, "kg")
+    elif pilihan == "b":
+        nilai = float(input("Masukkan nilai (ton): "))
+        hasil = nilai * 1000
+        print(nilai, "ton =", hasil, "kg")
+    elif pilihan == "c":
+        nilai = float(input("Masukkan nilai (gram): "))
+        hasil = nilai / 1000
+        print(nilai, "gram =", hasil, "kg")
+    elif pilihan == "d":
+        nilai = float(input("masukkan nilai hektogram (hg)"))
+        hasil = nilai / 10
+        print(nilai, "hg=", hasil, "kg")
+    elif pilihan == "e":
+        nilai = float(input("masukkan nilai dekagram (dag)"))
+        hasil = nilai / 100
+        print(nilai, "dag=", hasil, "kg")
+    else:
+        print("Pilihan tidak ada.")
+
+elif konversi == "3":
+    print("--- Konversi Suhu ---")
+    print("a. Celcius -> Kelvin")
+    print("b. Fahrenheit -> Kelvin")
+    print("c. Reamur -> Kelvin")
+    pilihan = input("Pilih konversi (a - c): ")
+
+    if pilihan == "a":
+        nilai = float(input("Masukkan suhu (C): "))
+        hasil = nilai + 273.15
+        print(nilai, "°C =", hasil, "K")
+    elif pilihan == "b":
+        nilai = float(input("Masukkan suhu (F): "))
+        hasil = (nilai - 32) * 5/9 + 273.15
+        print(nilai, "F =", hasil, "K")
+    elif pilihan == "c":
+        nilai = float(input("Masukkan suhu (R): "))
+        hasil = nilai * 5/4 + 273.15
+        print(nilai, "R =", hasil, "K")
+    else:
+        print("Pilihan tidak ada.")
+
+elif konversi == "4":
+    print("--- Konversi Waktu ---")
+    print("a. Menit -> Detik")
+    print("b. Jam -> Detik")
+    pilihan = input("Pilih konversi (a-b): ")
+
+    if pilihan == "a":
+        nilai = float(input("Masukkan nilai (menit): "))
+        hasil = nilai * 60
+        print(nilai, "menit =", hasil, "detik")
+    elif pilihan == "b":
+        nilai = float(input("Masukkan nilai (jam): "))
+        hasil = nilai * 3600
+        print(nilai, "jam =", hasil, "detik")
+    else:
+        print("Pilihan tidak ada.")
+
+elif konversi == "5":
+    print("--- Konversi Mata Uang ---")
+    print("a. Rupiah -> Dolar (USD)")
+    print("b. Dolar (USD) -> Rupiah")
+    print("c. Rupiah -> Yen (JPY)")
+    print("d. Yen (JPY) -> Rupiah")
+    print("e. Rupiah -> Won (KRW)")
+    print("f. Won (KRW) -> Rupiah")
+    pilihan = input("Pilih konversi (a-f): ")
+
+    if pilihan == "a":
+        nilai = float(input("Masukkan nilai (Rp): "))
+        hasil = nilai / 16703
+        print("Rp", nilai, "=", hasil, "USD")
+    elif pilihan == "b":
+        nilai = float(input("Masukkan nilai (USD): "))
+        hasil = nilai * 16703
+        print(nilai, "USD =", "Rp", hasil)
+    elif pilihan == "c":
+        nilai = float(input("Masukkan nilai (Rp): "))
+        hasil = nilai / 111.56
+        print("Rp", nilai, "=", hasil, "JPY")
+    elif pilihan == "d":
+        nilai = float(input("Masukkan nilai (JPY): "))
+        hasil = nilai * 111.56
+        print(nilai, "JPY =", "Rp", hasil)
+    elif pilihan == "e":
+        nilai = float(input("Masukkan nilai (Rp): "))
+        hasil = nilai / 11.83
+        print("Rp", nilai, "=", hasil, "KRW")
+    elif pilihan == "f":
+        nilai = float(input("Masukkan nilai (KRW): "))
+        hasil = nilai * 11.83
+        print(nilai, "KRW =", "Rp", hasil)
+    else:
+        print("Pilihan tidak ada.")
+
+else:
+    print("Kategori tidak ada.")
+
+
+
+
 
